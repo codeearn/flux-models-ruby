53a7cb20e0f804894151e5c4a3f436c80347f743
Gaurav Sablok <sablokg@gmail.com>
2024-03-26 12:30:42 +0100

    adding the ruby gem template

---
+++ cobragem.rb
@@ -0,0 +1,10 @@
+# usr/bin/ruby
+# Author Gaurav Sablok 
+# Universitat Potsdam
+# Date 2024-3-26
+
+def initialize(inputfile, outputfile, rates)
+	@inputfile = inputfile
+        @outputfile = outfile
+        @rates = rates
+end
